0d5bdea9d2c5c3f27e56947363803cf8589feece
BrianBleau <bleaubrian@gmail.com>
2021-05-31 10:51:06 -0400

    Mobile Adjustments

--- shared.js
+++ shared.js
@@ -2,6 +2,13 @@ const backdrop = document.querySelector(".backdrop");
 const selectPlanButtons = document.querySelectorAll(".plan button");
 const modal = document.querySelector(".modal");
 const noButton = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+const closeModal = () => {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+};
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
@@ -11,11 +18,15 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
 }
 
 backdrop.addEventListener("click", function () {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  mobileNav.style.display = "none";
+  closeModal();
 });
 
-noButton.addEventListener("click", function () {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+if (noButton) {
+  noButton.addEventListener("click", closeModal);
+}
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
 });
