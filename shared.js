b98b33a65e281f277dff1b46bbf97b15ab273a04
BrianBleau <bleaubrian@gmail.com>
2021-06-04 07:04:50 -0400

    Animations

--- shared.js
+++ shared.js
@@ -1,32 +1,54 @@
 const backdrop = document.querySelector(".backdrop");
-const selectPlanButtons = document.querySelectorAll(".plan button");
 const modal = document.querySelector(".modal");
-const noButton = document.querySelector(".modal__action--negative");
+const modalNoButton = document.querySelector(".modal__action--negative");
+const selectPlanButtons = document.querySelectorAll(".plan button");
 const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
 
-const closeModal = () => {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
-};
+// console.dir(backdrop.style['background-image']);
 
+// console.dir(backdrop);
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    modal.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
     backdrop.style.display = "block";
+    setTimeout(function () {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
   closeModal();
 });
 
-if (noButton) {
-  noButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+  setTimeout(function () {
+    backdrop.style.display = "none";
+  }, 500);
 }
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
   backdrop.style.display = "block";
+  setTimeout(function () {
+    backdrop.classList.add("open");
+  }, 10);
 });
